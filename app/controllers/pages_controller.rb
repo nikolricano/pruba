20bdebd63819368a0ed3dd69907257b719106ef3
Nikol <nricano87@gmail.com>
2016-11-22 05:25:28 -0600

    sin galeria

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def pagex
+    @user = User.new
+  end
+  def pagey
+    @user = User.create(name: params[:user][:name], age: params[:user][:age], email: params[:user][:email])
+    redirect_to pages_pagex_path, alert: "bien guardado"
+  end
+  def landing
+    render layout: 'landing'
   end
 end
